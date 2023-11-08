b21ae04451cb2bf118819b2eb620cdae1175db50
Mauricio-Velazquez <mauriciovelazquez211@gmail.com>
2023-11-08 17:54:28 -0300

    Practica 11 comienzo

---
+++ src/ar/edu/unq/po2/tp11/Song.java
@@ -0,0 +1,5 @@
+package ar.edu.unq.po2.tp11;
+
+public class Song {
+
+}
---
+++ test/ar/edu/unq/po2/tp11/SongTestCase.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.po2.tp11;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SongTestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
